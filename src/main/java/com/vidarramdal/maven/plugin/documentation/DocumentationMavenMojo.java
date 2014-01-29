72562dd3106bdea77860fa2274d4f48c90dc85c6
vramdal <vramdal@gmail.com>
2014-01-29 16:11:34 +0100

    Directory copy

--- src/main/java/com/vidarramdal/maven/plugin/documentation/DocumentationMavenMojo.java
+++ src/main/java/com/vidarramdal/maven/plugin/documentation/DocumentationMavenMojo.java
@@ -11,6 +11,7 @@ import org.codehaus.plexus.util.FileUtils;
 import org.markdown4j.Markdown4jProcessor;
 
 import java.io.File;
+import java.io.IOException;
 
 @Mojo( name = "sayhi", inheritByDefault = true, aggregator = true)
 public class DocumentationMavenMojo extends AbstractMojo {
@@ -24,6 +25,9 @@ public class DocumentationMavenMojo extends AbstractMojo {
     @Parameter( property = "sayhi.greeting", defaultValue = "Hello World!" )
     private String greeting;
 
+    @Parameter( property = "sayhi.outputDirectory", defaultValue = "documentation" )
+    private File _outputDir;
+
     @Parameter(property = "sayhi.documentationRoot", defaultValue = "documentation")
     private File _documentationRoot;
 	private Markdown4jProcessor markdown4jProcessor = new Markdown4jProcessor();
@@ -35,22 +39,54 @@ public class DocumentationMavenMojo extends AbstractMojo {
     public void execute() throws MojoExecutionException {
         getLog().info( "Hello, world." + greeting );
         getLog().info("Henter dokumentasjonsfiler fra " + this._documentationRoot.getAbsolutePath());
-		final File[] files = this._documentationRoot.listFiles();
-		for (File file : files) {
-			if ("md".equals(FileUtils.extension(file.getName()))) {
-//				String html = markdown4jProcessor.process(file);
+		File parentOutputDir = this._outputDir;
+		try {
+			processDirectory(this._documentationRoot, parentOutputDir);
+		} catch (Throwable e) {
+			throw new MojoExecutionException("Error creating documentation", e);
+		}
+	}
 
+	private void processDirectory(File inputDir, File parentOutputDir) throws IOException {
+		final File[] files = inputDir.listFiles();
+		if (files == null) {
+			throw new RuntimeException("Could not get files from " + this._documentationRoot.getAbsolutePath());
+		}
+		for (File file : files) {
+			final File targetFile = new File(parentOutputDir, file.getName());
+			if (file.isDirectory()) {
+				final boolean mkdirResult = targetFile.mkdir();
+				if (!mkdirResult) {
+					throw new RuntimeException("Could not create directory " + targetFile.getAbsolutePath());
+				}
+				processDirectory(file, targetFile);
+			} else if ("md".equals(FileUtils.extension(file.getName()))) {
+				String html = markdown4jProcessor.process(file);
+				FileUtils.fileWrite(targetFile, "UTF-8", html);
+			} else {
+				FileUtils.copyFile(file, targetFile);
 			}
 		}
 	}
 
-    public void setDocumentationRoot(File documentationRoot) {
-        if (!documentationRoot.isAbsolute()) {
-            documentationRoot = new File(project.getBasedir(), documentationRoot.getPath());
-        }
-		if (!documentationRoot.isDirectory()) {
-			throw new Error("DocumentationRoot should be a directory");
+	@SuppressWarnings("UnusedDeclaration")
+	public void setDocumentationRoot(File documentationRoot) {
+		this._documentationRoot = getAbsoluteDirectory(project.getBasedir(), documentationRoot, "documentationRoot");
+	}
+
+	@SuppressWarnings("UnusedDeclaration")
+	public void setOutputDirectory(File outputDirectory) {
+		this._outputDir = getAbsoluteDirectory(new File(project.getBuild().getDirectory()), outputDirectory, "outputDirectory");
+	}
+
+	private File getAbsoluteDirectory(File basedir, File absoluteOrRelative, String propertyName) {
+		if (!absoluteOrRelative.isAbsolute()) {
+			absoluteOrRelative = new File(basedir, absoluteOrRelative.getPath());
 		}
-        this._documentationRoot = documentationRoot;
-    }
+		if (!absoluteOrRelative.isDirectory()) {
+			throw new Error(propertyName + " should be a directory");
+		}
+		return absoluteOrRelative;
+	}
+
 }
